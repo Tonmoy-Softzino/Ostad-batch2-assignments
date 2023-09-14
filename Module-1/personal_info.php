07d6d11d2ba6806b7ab01512e3e87d2b863e56f1
Tonmoy pk <tonmoykumar78@gmail.com>
2023-09-14 22:31:02 +0600

    solve task 1

--- Module-1/personal_info.php
+++ Module-1/personal_info.php
@@ -1,3 +1,10 @@
 <?php
-    echo "hello"," world";
+$name = 'Tonmoy Kumar Pramanik';
+$age = 26;
+$country = 'Bangladesh';
+$brief_info = 'I am working as Associate Software Engineer ar Softzino Technology. I am working as Front-end developer but I want to be a full stack developer so that i enrolled Ostad. Hope this course help me to achive my goal';
+echo $name."\n";
+echo $age."\n";
+echo $country."\n";
+echo $brief_info."\n";
 ?>
